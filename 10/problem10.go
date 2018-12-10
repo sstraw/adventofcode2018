db18a5001f2e8223e486f9233443ddf2cf51ee40
sstraw <sean.r.straw@gmail.com>
2018-12-09 23:11:27 -0800

    Problem 10, cleaned up. I reviewed the reddit thread for the problem and saw several people with the idea to look for a low area bound as a possible message

--- 10/problem10.go
+++ 10/problem10.go
@@ -6,81 +6,94 @@ import (
     "bufio"
     "regexp"
     "strconv"
+    "bytes"
+    "flag"
 )
 
-
-type Point struct {
+type Star struct {
     X, Y, Xv, Yv int
 }
 
-type Points []*Point
-func (p Points) Len() int { return len(p) }
-func (p Points) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
-func (p Points) Less(i, j int) bool {
-    if p[i].X < p[j].X { return true }
-    if p[i].X > p[j].X { return false }
-    if p[i].Y < p[j].Y { return true }
-    return false
+type Sky struct {
+    Stars []*Star
+    MinX, MinY, MaxX, MaxY, Area int
 }
 
 func main () {
+    limArea := flag.Int("a", 600, "Max area coverage of stars for possible message")
+    limMatch:= flag.Int("m", 1, "Max number of matches to look for. -1 for unlimited")
+    flag.Parse()
+
     scanner := bufio.NewScanner(os.Stdin)
     re := regexp.MustCompile("-?\\d+")
 
-    points := make([]*Point, 0)
+    sky := &Sky{}
 
     for scanner.Scan() {
         m := re.FindAllString(scanner.Text(), -1)
-        p := &Point{}
-        p.X , _ = strconv.Atoi(m[0])
-        p.Y , _ = strconv.Atoi(m[1])
-        p.Xv, _ = strconv.Atoi(m[2])
-        p.Yv, _ = strconv.Atoi(m[3])
-        points = append(points, p)
+        s := &Star{}
+        s.X , _ = strconv.Atoi(m[0])
+        s.Y , _ = strconv.Atoi(m[1])
+        s.Xv, _ = strconv.Atoi(m[2])
+        s.Yv, _ = strconv.Atoi(m[3])
+        sky.Stars = append(sky.Stars, s)
     }
 
-    for i := 0; ;i++ {
-        minX, minY, maxX, maxY := 99999999, 99999999, 0, 0
-        for _, p := range points {
-            if p.X < minX { minX = p.X }
-            if p.Y < minY { minY = p.Y }
-            if p.X > maxX { maxX = p.X }
-            if p.Y > maxY { maxY = p.Y }
+    nMatches := 0
+    sky.Update()
+    for i := 1; nMatches != *limMatch; i++ {
+        if sky.Area < *limArea {
+            fmt.Println("Found possible message at second", i)
+            fmt.Println(sky)
+            nMatches++
         }
-
-        area := (maxX-minX)*(maxY-minY)
-
-        if area < 600 {
-            fmt.Println("Found possible message", i)
-            for y := minY; y <= maxY; y++ {
-                for x := minX; x <=maxX; x++ {
-                    var p *Point
-                    for _, v := range points {
-                        if v.X == x && v.Y == y {
-                            p = v
-                            break
-                        }
-                    }
-                    if p == nil {
-                        fmt.Print(".")
-                    } else {
-                        fmt.Print("#")
-                    }
-                }
-                fmt.Println()
-            }
-        }
-        for _, p := range points {
-            p.Move()
-        }
+        sky.Update()
     }
 }
 
-func (p Point) String() string {
+func (s *Sky) Update() {
+    p := s.Stars[0]
+    s.MinX, s.MaxX, s.MinY, s.MaxY = p.X, p.X, p.Y, p.Y
+    for _, p := range s.Stars {
+        p.Move()
+        if p.X < s.MinX { s.MinX = p.X }
+        if p.Y < s.MinY { s.MinY = p.Y }
+        if p.X > s.MaxX { s.MaxX = p.X }
+        if p.Y > s.MaxY { s.MaxY = p.Y }
+    }
+    s.Area = (s.MaxX - s.MinX) * (s.MaxY - s.MinY)
+}
+
+
+func (p Star) String() string {
     return fmt.Sprintf("X: %3v Y: %3v Xv: %3v Yv: %3v", p.X, p.Y, p.Xv, p.Yv)
 }
 
-func (p *Point) Move () {
+func (p *Star) Move () {
     p.X += p.Xv
     p.Y += p.Yv
 }
+
+func (s Sky) String() string {
+    var buf bytes.Buffer
+
+    buf.WriteString(fmt.Sprintf("Area: %v MinX: %v MaxX: %v MinY: %v MaxY: %v", s.Area, s.MinX, s.MaxX, s.MinY, s.MaxY))
+    for y := s.MinY; y <= s.MaxY; y++ {
+        buf.WriteString("\n")
+        for x := s.MinX; x <= s.MaxX; x++ {
+            taken := false
+            for _, p := range s.Stars {
+                if x == p.X && y == p.Y {
+                    buf.WriteString("#")
+                    taken = true
+                    break
+                }
+            }
+            if ! taken {
+                buf.WriteString(".")
+            }
+        }
+    }
+    return buf.String()
+}
+

4457577f053de1d84df09aafc43589584e4547dc
sstraw <sean.r.straw@gmail.com>
2018-12-12 00:38:26 -0800

    Needed to reference other solutions to realize extrapolation is possible. Code could use updating to actually solve it instead of provide data to solve it.

--- 12/problem12.go
+++ 12/problem12.go
@@ -15,7 +15,7 @@ func main () {
     input := re.FindAllString(scanner.Text(), -1)[0]
     scanner.Scan() // Blank line
 
-    buffLen := (2 * 20) + 2 // Can only spread 2 * gen per gen, each side
+    buffLen := (2 * 2000) + 2 // Can only spread 2 * gen per gen, each side
 
     pots := make([]byte, len(input) + (2 * buffLen))
 
@@ -35,20 +35,20 @@ func main () {
         patterns[m[0]] = byte(m[1][0])
     }
 
-    for gen := 0; gen < 20; gen++ {
+    for gen := 0; gen < 2000; gen++ {
         newPots := make([]byte, len(pots))
         newPots[0], newPots[1], newPots[len(pots)-1], newPots[len(pots)-2] = 46, 46, 46, 46
+        sum := 0
         for i := 2; i < (len(pots)-2); i++ {
             newPots[i] = patterns[string(pots[i-2:i+3])]
+            if newPots[i] == byte('#') {
+                sum += i - buffLen
+            }
         }
+
         pots = newPots
+
+        fmt.Printf("Generation %4v: %9v\n", gen, sum)
     }
 
-    sum := 0
-    for i, v := range pots {
-        if v == byte('#') {
-            sum += i - buffLen
-        }
-    }
-    fmt.Println("Sum of pots", sum)
 }
